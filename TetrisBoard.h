2ae7a8ba233dd8f2e806a7697d24a4848646c105
vung2k2 <81573785+vung2k2@users.noreply.github.com>
2021-05-11 09:47:16 +0700

    Update TetrisBoard.h

--- TetrisBoard.h
+++ TetrisBoard.h
@@ -3,6 +3,8 @@
 #include <time.h>
 #include <SDL.h>
 #include <iostream>
+#include <sstream>
+#include <fstream>
 
 #include "Texture.h"
 #include "Color.h"
@@ -13,14 +15,14 @@
 #define BOARD_WIDTH 12
 #define BOARD_HEIGHT 21
 #define BLOCK_SIZE 35
-#define PER_FRAME 25
+
 
 class TetrisBoard
 {
 	public:
 		TetrisBoard();
 		~TetrisBoard();
-
+        int score,scoreMax;
 		void randomizeBoard();
 		void initBoard();
 		void draw(SDL_Renderer* renderer);
@@ -32,16 +34,19 @@ class TetrisBoard
 		void resetGame();
 		void handleInput(SDL_Event e);
         bool GameOver();
+        void luu_diem();
 
 	private:
+	    int PER_FRAME= 25;
+
 		bool canPlace(int newPosX, int newPosY);
 		bool canRotate();
 		bool isLineFull(int i);
 
 		Input _input;
-
 		int _frame;
 		Texture _block;
+
 		int _posX;
 		int _posY;
 		int _piecePosX;
@@ -50,4 +55,10 @@ class TetrisBoard
 		Color _piece[4][4];
 		Color _boardArray[BOARD_WIDTH][BOARD_HEIGHT];
 };
+std::string int_to_string(int x);
+
+void loadText(std::string s, SDL_Renderer* renderer, int _size, int x, int y);
+
+
+
 
