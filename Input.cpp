7095c50c07877e09d6d98dd5abc4ad8217c5bb0d
vung2k2 <81573785+vung2k2@users.noreply.github.com>
2021-05-11 09:45:56 +0700

    Update Input.cpp

--- Input.cpp
+++ Input.cpp
@@ -1,39 +1,56 @@
-#pragma once
-#include <SDL.h>
-#include <stdio.h>
+#include "Input.h"
 
-enum ArrowKeys
+
+
+Input::Input()
 {
-	LEFT,
-	RIGHT,
-	UP,
-	DOWN,
-	RESET,
-	EXIT,
-	EASY,
-	MEDIUM,
-	HARD
-};
+	_upPressed = false;
+	_downPressed = false;
+	_leftPressed = false;
+	_rightPressed = false;
 
-class Input
+	_leftFrame = 0;
+	_rightFrame = 0;
+
+	for (int i = 0; i < 322; i++) { // init them all to false
+		KEYS[i] = false;
+	}
+}
+
+
+Input::~Input()
 {
-	bool KEYS[322];
+}
 
-	public:
-		Input();
-		~Input();
+ArrowKeys Input::getPress(SDL_Event e)
+{
+	if (e.type == SDL_KEYDOWN)
+	{
+		switch (e.key.keysym.sym)
+		{
+			case SDLK_r:
+			    return RESET;
 
-		ArrowKeys getPress(SDL_Event e);
+			case SDLK_UP:
+				return UP;
 
-	private:
-		bool _leftPressed;
-		bool _rightPressed;
-		bool _upPressed;
-		bool _downPressed;
-		bool _rPressed;
+			case SDLK_DOWN:
+				return DOWN;
 
-		int _downFrame;
-		int _rightFrame;
-		int _leftFrame;
-};
+			case SDLK_LEFT:
+				return LEFT;
 
+			case SDLK_RIGHT:
+				return RIGHT;
+
+            case SDLK_ESCAPE:
+                return EXIT;
+            case SDLK_1:
+                return EASY;
+            case SDLK_2:
+                return MEDIUM;
+            case SDLK_3:
+                return HARD;
+		}
+	}
+}
